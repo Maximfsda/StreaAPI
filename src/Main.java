00c619b52a868d33724a54c323c017107c52190a
Maximfsda <lisitsin.maxi@yandex.ru>
2023-02-01 15:37:10 +1000

    taskOne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    // Задание 1
+
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> items = stream.sorted(order).collect(Collectors.toList());
+        if (!items.isEmpty()) {
+            minMaxConsumer.accept(items.get(0), items.get(items.size() - 1));
+
+        } else {
+            minMaxConsumer.accept(null, null);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Stream<Integer> stream = new ArrayList<>(Arrays.asList(1, 2, 3, 5, 8, 13, 21)).stream();
+
+        findMinMax(
+                stream,
+                (x, y) -> x.compareTo(y),
+                (x, y) -> System.out.println(String.format("min: %s, max: %s", x, y))
+        );
+
+        stream.close();
     }
 }
